23059ecd06235996cc7048f865488151aea8e80f
İlhan Can Tuğcu <ilhancantugcu@hotmail.com>
2025-12-02 17:53:56 +0300

    Update

---
+++ Core_Proje/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class AdminNavbarMessageList : ViewComponent
+    {
+        MessageManager messageManager2 = new MessageManager(new EfMessageDal());
+        public IViewComponentResult Invoke()
+        {
+
+            var values = messageManager2.TGetList().Where(x => x.Status == true)
+                .OrderByDescending(x => x.MessageID).Take(3).ToList();
+
+            ViewBag.count = values.Count();
+
+            return View(values);
+        }
+    }
+}
